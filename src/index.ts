ea789a6bfe44ab1163f8e882070264401c9b34bf
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2021-10-15 23:13:19 -0400

    refactor (main): optimizo funcion principal

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import {Character} from './class/Character';
 
-export function main(): void {
+(() => {
   const character = new Character('2', 'Michaell');
   console.debug(character);
-}
-
-main();
+})();
